6bfca3cae5fc241a5ef9fcced212afc02cf1cdbb
John Otander <johnotander@gmail.com>
2014-11-23 12:25:01 -0700

    Add config

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+var concatFiles = require('broccoli-concat');
+var removeFile = require('broccoli-file-remover');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-css-concat'
+  name: 'ember-cli-css-concat',
+
+  config: function(/* environment, appConfig */) {
+    return {
+      cssConcat: {
+        inputFiles: [
+          'assets/' + this.app.name + '.css',
+          'assets/vendor.css'
+        ],
+        outputFile: '/assets/' + this.app.name + '.css'
+      }
+    };
+  }
 };
