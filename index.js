fa12683cf97ceca8b3c9079c1047b8e94458cef0
John Otander <johnotander@gmail.com>
2014-11-28 15:50:28 -0700

    Register css concatenation and add toTree method

--- index.js
+++ index.js
@@ -17,5 +17,26 @@ module.exports = {
         outputFile: '/assets/' + this.app.name + '.css'
       }
     };
+  },
+
+  included: function(app) {
+    this._super.include(app);
+
+    app.registry.add('css', {
+      name: 'ember-cli-css-concat',
+      ext: 'css',
+      toTree: function(tree) {
+        var concatedFiles = concatFiles(tree, {
+          inputFiles: ['assets/' + app.name + '.css', 'assets/vendor.css'],
+          outputFile: '/assets/' + app.name + '.css'
+        });
+
+        var appTreeWithoutStyles = removeFile(tree, {
+          files: ['assets/vendor.css']
+        });
+
+        return mergeTrees([appTreeWithoutStyles,  compiledStyles], { overwrite: true });
+      }
+    })
   }
 };
